c7edba45d3911e2e64f3a383055c756d6b801d95
Ryan Macalandag <rmacalandag@gmail.com>
2024-03-24 18:28:12 +1100

    fix accordion borders

--- src/app/parts/SectionJoin.tsx
+++ src/app/parts/SectionJoin.tsx
@@ -12,7 +12,7 @@ function SectionJoin() {
         </div>
         <p className=''>Community building isn&apos;t easy, especially when you are operating without funding.</p>
         
-        <div className='flex flex-col my-8 border-2 rounded-xl *:border-b-2 last-of-type:border-b-0 overflow-hidden'>
+        <div className='flex flex-col my-3 md:my-8 border-2 rounded-xl overflow-hidden *:border-b-2'>
           <AccordionItem 
             title="Eligibility"
             content="Community building isn&apos;t easy, especially when you are operating without funding and are relying on volunteer time. But hearing the feedback like the above makes us think our efforts are worth it, knowing that we can have a positive impact on others and even inspire them to think of ways on how to help others as well. Regardless of your age, status, gender, or background, you can find a way to pay it forward and help people in our community."
@@ -23,12 +23,12 @@ function SectionJoin() {
           />
         </div>
 
-        <a href="#about" className='flex flex-row justify-between items-center gap-8 w-[16rem] hover:w-[20rem] font-heading text-sm font-semibold uppercase my-12 px-4 md:px-8 py-4 bg-indigo-700 hover:bg-indigo-900 border-2 border-indigo-700 hover:border-indigo-900 hover:shadow-glow text-indigo-100 hover:text-white rounded-lg tracking-wider transition-all duration-300'>
+        <a href="#about" className='flex flex-row justify-between items-center gap-8 w-[16rem] hover:w-[20rem] font-heading text-sm font-semibold uppercase my-8 px-4 md:px-8 py-4 bg-indigo-700 hover:bg-indigo-900 border-2 border-indigo-700 hover:border-indigo-900 hover:shadow-glow text-indigo-100 hover:text-white rounded-lg tracking-wider transition-all duration-300'>
           <span>Signup form</span>
           <span>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" /></svg>
           </span>
-          </a>
+        </a>
 
       </div>
 
