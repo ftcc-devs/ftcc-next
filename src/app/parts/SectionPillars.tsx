8a9c649854228b0e3bf18cc8940bc90a5617c496
Ryan Macalandag <rmacalandag@gmail.com>
2024-04-11 21:44:38 +1000

    pillar cards hover

--- src/app/parts/SectionPillars.tsx
+++ src/app/parts/SectionPillars.tsx
@@ -7,7 +7,7 @@ function SectionPillars() {
 
   const pillarslist = 
     pillars.map((p) => 
-      <div key={p.id} className='grid grid-cols-4 pt-8 md:pt-12 pb-4 md:pb-8 px-4 md:px-6 bg-slate-100 dark:bg-slate-800 rounded-lg gap-2 md:gap-4'>
+      <div key={p.id} className='grid grid-cols-4 pt-8 md:pt-12 pb-4 md:pb-8 px-4 md:px-6 bg-slate-100 hover:bg-indigo-400/10 dark:bg-slate-800 hover:-translate-y-2 rounded-lg gap-2 md:gap-4 transition duration-300'>
         <div className='flex flex-row justify-center sm:justify-start'>
           <Image
             src={p.icon}
